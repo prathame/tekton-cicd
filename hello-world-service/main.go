8139dc2d18fc6adecf2dc1339ced745d5d5f3544
prathame <prathamesh.busa@cldcvr.com>
2024-02-12 13:00:32 +0530

    commit

---
+++ hello-world-service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	fmt.Println("Server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
